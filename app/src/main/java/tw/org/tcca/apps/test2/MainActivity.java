15302f4126a53c3bb5fb00d693dd97a480189363
bradchao <brad@brad.tw>
2020-09-19 11:24:51 +0800

    2

--- app/src/main/java/tw/org/tcca/apps/test2/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test2/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private ConnectivityManager cmgr;
@@ -15,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cmgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        Log.v("bradlog", "1. " + isConnectNetwork());
+        Log.v("bradlog", "2. " + isWifiConnected());
+    }
+
+    private boolean isConnectNetwork(){
+        NetworkInfo networkInfo = cmgr.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    private boolean isWifiConnected(){
+        NetworkInfo networkInfo = cmgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return networkInfo.isConnected();
     }
 }

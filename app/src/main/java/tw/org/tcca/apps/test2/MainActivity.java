4d362dd267d56fc5edad452dffa71ce165cf3850
bradchao <brad@brad.tw>
2020-09-19 11:06:30 +0800

    1

---
+++ app/src/main/java/tw/org/tcca/apps/test2/MainActivity.java
@@ -0,0 +1,19 @@
+package tw.org.tcca.apps.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ConnectivityManager cmgr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cmgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+}
